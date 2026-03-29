3b11861bdbee1eb70aed0a852e9489b33ad16b3d
Shorouk ELSayed <shoroukelsayed2545@gmail.com>
2026-03-30 00:00:17 +0200

    first commit

---
+++ lib/features/tour/presentation/view/tour_list_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TourListView extends StatelessWidget {
+  const TourListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
